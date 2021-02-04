0e7996b021044ce93b0d68343b48a9af0d828ffe
sachin <sachin@gmail.com>
2021-02-04 16:27:59 +0530

    add dto for method argument

---
+++ src/main/java/com/ayansys/test/Order.java
@@ -0,0 +1,10 @@
+package com.ayansys.test;
+
+import lombok.Data;
+
+@Data
+public class Order {
+private String userName;
+private Double amount;
+private String productName;
+}
---
+++ src/test/java/com/ayansys/test/GitTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayansys.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
